db01906c038f608448c8de5ff5e4e0935f8e04a0
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-22 15:57:10 +0300

    WpfApp2_2_pract(123)

--- WpfApp2-2-3/MainWindow.xaml.cs
+++ WpfApp2-2-3/MainWindow.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp2_2
+namespace WpfApp2_2_3
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class MainWindow : Window
     {
-        public Window1()
+        public MainWindow()
         {
             InitializeComponent();
-            
         }
     }
 }
